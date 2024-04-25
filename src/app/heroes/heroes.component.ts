2c6912ade3f86b5fb20f536f118605f87ed87eee
zlatansmail <66694454+zlatansmail@users.noreply.github.com>
2024-04-25 16:52:00 +0200

    finished app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  /*
+  hero = 'Windstorm';
+  */
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
